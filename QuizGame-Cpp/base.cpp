6cf9133d5ff7b84561d2a8134a43f22a0489f36b
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 19:07:29 +0530

    test1

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -2,6 +2,10 @@
 #include <string>
 using namespace std;
 int main () {
+        cout << "------------- WELCOME TO MY C++ QUIZ-----------------" << endl;
+        cout << "---------You need to answer 10 questions from here (4 Pass marks)----------" << endl;
+}
+int star_quiz () {
     int score=0;
     char x; char s,S;
     cin >> x;
