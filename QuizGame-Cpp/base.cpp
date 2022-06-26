5264a4c292e9b84bd9e85bd4ac60c7e49a03c9d4
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 19:35:24 +0530

    init3

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int start_quiz () {
     int score=0;
     char x; char s,S;
-    cin >> x;
+    cin >> x;                           /*Call fuction*/
     if (x=='s'||x=='S')
     {
-        cout << "GOOD LUCK";
+        cout << "GOOD LUCK" << endl << endl;
+        cout << "Q.1 A = x% of y and B = y% of x, then which of the following is true? \n" << "A is smaller than B." << endl;
     }
     else {
         cout << "Try again";
