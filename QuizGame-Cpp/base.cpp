460449a3e30946b2290e1bac26a8b929576f77f3
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 19:41:50 +0530

    init4

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -7,8 +7,7 @@ int start_quiz () {
     cin >> x;                           /*Call fuction*/
     if (x=='s'||x=='S')
     {
-        cout << "GOOD LUCK" << endl << endl;
-        cout << "Q.1 A = x% of y and B = y% of x, then which of the following is true? \n" << "A is smaller than B." << endl;
+        std::cout << "Q1 A = x% of y and B = y% of x, then which of the following is true? \n" << "1. A is smaller than B.\n" << "2. A is greater than B. \n" << "3. If x is smaller than y, then A is greater than B..\n" << "4. A is equal to B."<< endl;
     }
     else {
         cout << "Try again";
@@ -16,9 +15,10 @@ int start_quiz () {
     return 0;
 }
 int main () {
-        cout << "-----------------WELCOME TO MY C++ QUIZ-----------------" << endl;
+        cout << "--------------------------WELCOME TO MY C++ QUIZ---------------------------" << endl;
         cout << "---------You need to answer 10 questions from here (4 Pass marks)----------" << endl;
-        cout << "-------You will get +1 credit for each correct answer, here is no negetive marking-------" << endl;
-        cout << "----------To start this quiz press 'S'----------" << endl;
+        cout << "You will get +1 credit for each correct answer, here is no negetive marking" << endl;
+        cout << "--------------------To start this quiz press 'S'---------------------------" << endl;
+        cout << "------------------------------ALL THE BEST---------------------------------" << endl;
         start_quiz ();
 }
