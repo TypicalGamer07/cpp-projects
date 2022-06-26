915540608caf6948a055e24de95389fb52ec0946
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 19:24:38 +0530

    init2

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main () {
-        cout << "------------- WELCOME TO MY C++ QUIZ-----------------" << endl;
-        cout << "---------You need to answer 10 questions from here (4 Pass marks)----------" << endl;
-}
-int star_quiz () {
+int start_quiz () {
     int score=0;
     char x; char s,S;
     cin >> x;
     if (x=='s'||x=='S')
     {
-        cout << "OK";
+        cout << "GOOD LUCK";
     }
     else {
-        cout << "not ok";
+        cout << "Try again";
     }
+    return 0;
+}
+int main () {
+        cout << "-----------------WELCOME TO MY C++ QUIZ-----------------" << endl;
+        cout << "---------You need to answer 10 questions from here (4 Pass marks)----------" << endl;
+        cout << "-------You will get +1 credit for each correct answer, here is no negetive marking-------" << endl;
+        cout << "----------To start this quiz press 'S'----------" << endl;
+        start_quiz ();
 }
