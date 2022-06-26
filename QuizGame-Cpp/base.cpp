a71bb40686c495257c2d3053883def7d1fe1b41c
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 20:11:02 +0530

    init6

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -7,16 +7,36 @@ int start_quiz () {
     cin >> x;                           /*Call fuction*/
     if (x=='s'||x=='S')
     {
-        std::cout << "Q1 A = x% of y and B = y% of x, then which of the following is true? \n" << "1. A is smaller than B.\n" << "2. A is greater than B. \n" << "3. If x is smaller than y, then A is greater than B..\n" << "4. A is equal to B."<< endl;
-        cin >> option;
+        std::cout << "Q1 A = x% of y and B = y% of x, then which of the following is true? \n" << "1. A is smaller than B.\n" << "2. A is greater than B. \n" << "3. If x is smaller than y, then A is greater than B.\n" << "4. A is equal to B."<< endl;
+        cout << "Your answer ==> "; cin >> option;
         if (option==4)
         {
-            cout << "CORRECT !" << endl;
+            cout << "CORRECT !" << endl<< endl;
             score=score+1;
+        } 
+        else if (option>4)
+        {
+            cout << "Option is not valid" << endl << endl;
         }
         else
         {
-            cout << "WRONG !!" << endl;
+            cout << "WRONG !!" << endl<< endl;
+            score=score;
+        }
+        cout << "Q2. Which of the following states is not located in the North? \n" << "1. Jharkhand.\n" << "2. Jammu and Kashmir. \n" << "3. Himachal Pradesh.\n" << "4. Haryana."<< endl;
+        cout << "Your answer ==> "; cin >> option;
+        if (option==1)
+        {
+            cout << "CORRECT !" << endl<< endl;
+            score=score+1;
+        }
+        else if (option>4)
+        {
+            cout << "Option is not valid" << endl<< endl;
+        }
+        else
+        {
+            cout << "WRONG !!" << endl<< endl;
             score=score;
         }
     cout << "Your overall score==>  " << score << "/10" << endl;         
