f665aa4941ec8f5231b6d906af927e4df1781eb2
Subhabrata <subhabratasen1999@gmail.com>
2022-06-26 19:01:50 +0530

    test

--- QuizGame-Cpp/base.cpp
+++ QuizGame-Cpp/base.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main () {
-    
-}
-int star_quiz () {
-    
+    int score=0;
+    char x; char s,S;
+    cin >> x;
+    if (x=='s'||x=='S')
+    {
+        cout << "OK";
+    }
+    else {
+        cout << "not ok";
+    }
 }
