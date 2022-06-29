87294daa518b3b6c20f79511a7bda09fffa9864b
Subhabrata <subhabratasen1999@gmail.com>
2022-06-29 18:12:59 +0530

    push

--- Cpp Calculator/main.cpp
+++ Cpp Calculator/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main_calc () {
+    char start;
+    cout << "Press any key to continue" << endl;
+    system("read");     // For Unix
+    //system("pause");     For Windows
     return 0;
 }
 int main () {
-    
+        cout << "----WELCOME to my C++ Calculator------" << endl;
+        cout << "---------------It's free--------------" << endl;
+        main_calc ();
 }
